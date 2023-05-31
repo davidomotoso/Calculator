ea2118822343318adb2a7e2f75a996b8df9dc318
davidomotoso <117993843+davidomotoso@users.noreply.github.com>
2023-05-31 08:23:54 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# This is a Calculator app.
+
+## Description
+
+This is a basic calculator built with JavaScript,CSS and HTML. It provides basic functions like multiplication,division,addition and substraction.This app was built as a beginner task <h5>Intro to JavaScript</h5>
+
+## Features
+
+- Responsive design
+
+- Accepts mathematical instructions from users.
+
+- Displays the output of their instructions.
+
+- Stores the output and returns it back to the user when needed when the ans button is clicked.
+
+- Consist of a clear and delete button, both performing different functions which would be explained in the
+
+#### usage.
+
+## Installation
+
+#### 1 Clone the repository
+
+```
+https://github.com/davidomotoso/Calculator.git
+```
+
+- Navigate to the Calculator folder
+- Open calculator.html in your browser
+
+## Usage
+
+- Click on the calculator button to display the calculator.
+- Click on the ans button to return the output to the user.
+- Click on the clear button to clear the screen.
+- Click on the delete button to remove the last instruction or output.
+
+## Liked this template? Buy me a coffe
+
+### Author: <a href='https://github.com/davidomotoso'>David Omotoso</a>.
---
+++ calculator.js
@@ -0,0 +1,82 @@
+const htmlFile = () => {
+  const body = document.body;
+  body.innerHTML = `  <div>
+    <label for="value">
+      <textarea
+        name="value"
+        id=""
+        cols="30"
+        rows="6"
+        readonly
+        placeholder="Type in your Equation"
+      ></textarea>
+    </label>
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <button
+              type="submit"
+              title="Clear"
+              id="btn-clear"
+            >
+              C
+            </button>
+          </td>
+          <td><button onclick=action("/") type="submit">/</button></td>
+          <td><button onclick=action("*")  type="submit">X</button></td>
+          <td><button type="submit"id="btn-delete">Del</button></td>
+        </tr>
+        <tr>
+          <td><button onclick=action(7) type="submit">7</button></td>
+          <td><button type="submit" onclick=action(8)>8</button></td>
+          <td><button type="submit" onclick=action(9)>9</button></td>
+          <td><button type="submit" onclick=action("-")>-</button></td>
+        </tr>
+        <tr>
+          <td><button type="submit" onclick=action(4)>4</button></td>
+          <td><button type="submit" onclick=action(5)>5</button></td>
+          <td><button type="submit" onclick=action(6)>6</button></td>
+          <td><button type="submit" onclick=action("+")>+</button></td>
+        </tr>
+        <tr>
+          <td><button type="submit" onclick=action(1)>1</button></td>
+          <td><button type="submit" onclick=action(2)>2</button></td>
+          <td><button type="submit" onclick=action(3)>3</button></td>
+          <td><button type="submit"id="btn-answer">Ans</button></td>
+        </tr>
+        <tr>
+          <td><button type="submit" onclick=action("%")>%</button></td>
+          <td><button type="submit" onclick=action(0)>0</button></td>
+          <td><button type="submit" onclick=action(".")>.</button></td>
+          <td><button type="submit"id="btn-equal">=</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  `;
+};
+htmlFile();
+
+const clear = document.querySelector("#btn-clear");
+const textarea = document.querySelector("textarea");
+const deletion = document.querySelector("#btn-delete");
+const answer = document.querySelector("#btn-answer");
+const equal = document.querySelector("#btn-equal");
+clear.addEventListener("click", () => {
+  textarea.value = "";
+});
+const action = (value) => {
+  textarea.value += value;
+};
+deletion.addEventListener("click", () => {
+  let textVal = textarea.value;
+  textarea.value = textVal.slice(0, -1);
+});
+equal.addEventListener("click", () => {
+  textarea.value = eval(textarea.value);
+  localStorage.setItem("ans", textarea.value);
+});
+answer.addEventListener("click", () => {
+  textarea.value = localStorage.getItem("ans");
+});
